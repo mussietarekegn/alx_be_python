5804340d4c3150c91322ff5325e664cf4cabc9ed
Mussie Tarekegn Baye <mussie.tarekegn@aastustudent.edu.et>
2025-09-20 09:46:16 -0700

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,7 +2,7 @@ weather=input("What's the weather like today? (sunny/rainy/cold):")
 if weather.lower()=="sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather.lower()=="rainy":
-    print("Don't forget your umbrella and a raincoat")
+    print("Don't forget your umbrella and a raincoat.")
 elif weather.lower()=="cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
