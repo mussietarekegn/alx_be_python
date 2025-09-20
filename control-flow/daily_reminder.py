c6f69587fcfa4172613469909bec5f99b0f142d2
Mussie Tarekegn Baye <mussie.tarekegn@aastustudent.edu.et>
2025-09-20 12:29:26 -0700

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,27 @@
-task=str(input("Enter the task description: "))
-priority=str(input("Enter the task's priority(high,medium,low): "))
-time_bound=str(input("Is the task in time_bound(yes or no): "))
+
+task = input("Enter your task: ").strip()
+priority = input("Priority (high/medium/low): ").strip().lower()
+time_bound = input("Is it time-bound? (yes/no): ").strip().lower()
+
 
 match priority:
     case "high":
-        message=f"Reminder: '{task}' is a high priority task"
+        message = f"Reminder: '{task}' is a high priority task"
     case "medium":
-        message=f"Reminder: '{task}' is a medium priority task"
+        message = f"Reminder: '{task}' is a medium priority task"
     case "low":
-        message=f"Note : '{task}' is a low priority task"
+        message = f"Note: '{task}' is a low priority task"
     case _:
-        message:f"'{task}' has an unknown priority level"
+        message = f"'{task}' has an unknown priority level"
 
-if time_bound=="yes":
-    message+="that requires immediate attention today!"
+
+if time_bound == "yes":
+    message += " that requires immediate attention today!"
 else:
-    if priority in ["high","medium"]:
-        message+=". Consider completing it as soon as possible."
+    if priority in ["high", "medium"]:
+        message += ". Consider completing it as soon as possible."
     else:
-        message+=". Consider completing it when you have free time."
+        message += ". Consider completing it when you have free time."
+
+
 print(message)
