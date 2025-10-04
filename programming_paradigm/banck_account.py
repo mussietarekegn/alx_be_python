748e7fd07a63bd2c5ad7857d0bb3bda0cb848f38
mussietarekegn <mussie.tarekegn@aastustudent.edu.et>
2025-10-04 22:01:45 +0300

    Add BanckAccount class and CLI main-0.py

---
+++ programming_paradigm/banck_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self,initial_balance: float=0.0):
+        self.account_balance=float(initial_balance)
+    
+    def deposit(self,amount:float):
+        self.account_balance+=float(amount)
+    
+    def withdraw(self,amount:float):
+        amount=float(amount)
+        if amount<=self.account_balance:
+            sefl.account_balance-=amount
+            return True
+        return False
+    
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance:.2f}")
